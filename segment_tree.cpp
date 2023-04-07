517b8656d4e75b062556919480cd50cf97b34a83
Quan Duong <quanduong1396@gmail.com>
2023-04-07 10:03:16 +0700

    Update segment_tree.cpp

--- segment_tree.cpp
+++ segment_tree.cpp
@@ -7,7 +7,7 @@ void build(int node, int l=1, int r=n)
     else {
         int mid = (l+r)>>1;
         build(node*2, l, mid);
-        build(node*2+1, mod+1, r);
+        build(node*2+1, mid+1, r);
         
         st[node] = st[node*2] + st[node*2+1]; 
     }
