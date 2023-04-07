43528a5bcef354b1beb2c5158af58b81a3379c0c
Quan Duong <quanduong1396@gmail.com>
2023-04-07 10:30:44 +0700

    Create bs.cpp

---
+++ bs.cpp
@@ -0,0 +1,17 @@
+bool check(int vl, x)
+{
+    return vl >= x;
+}
+
+void solve()
+{
+    int arr[] = {...};
+    int x, l=arr[0], r=arr[n];
+    
+    while (l < r)
+    {
+        int mid = (l+r)>>1;
+        if (check(mid,x)) r = mid;
+        else l = mid+1;
+    }
+}
