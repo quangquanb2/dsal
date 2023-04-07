139ca3b3b8ed1807139a70abed3fdba49be0d34a
Quan Duong <quanduong1396@gmail.com>
2023-04-07 10:10:29 +0700

    Update segment_tree.cpp

--- segment_tree.cpp
+++ segment_tree.cpp
@@ -18,7 +18,7 @@ int querySum(int node, int l, int r, int tl, int tr)
     if (l <= tl && tr <= r) return st[node];
     if (tl > r || tr < l) return 0;
     int mid = (tl+tr)>>1;
-    return querySum(node*2, l, r, tl, mid) + querySum(ode*2+1, l, r, mid+1, tr);
+    return querySum(node*2, l, r, tl, mid) + querySum(node*2+1, l, r, mid+1, tr);
 }
 
 void update(int node, int tl, int tr, int k, int vl)
