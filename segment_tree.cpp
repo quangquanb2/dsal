03d8cb306ed053aa90945c3699483cb522b37092
Quan Duong <quanduong1396@gmail.com>
2023-04-07 10:09:47 +0700

    Update segment_tree.cpp

--- segment_tree.cpp
+++ segment_tree.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void build(int node, int l=1, int r=n)
+void buildSum(int node, int l=1, int r=n)
 {
     if (l == r) st[node] = arr[l];
     else {
@@ -10,6 +10,26 @@ void build(int node, int l=1, int r=n)
         build(node*2+1, mid+1, r);
         
         st[node] = st[node*2] + st[node*2+1]; 
+    }
+}
+
+int querySum(int node, int l, int r, int tl, int tr)
+{
+    if (l <= tl && tr <= r) return st[node];
+    if (tl > r || tr < l) return 0;
+    int mid = (tl+tr)>>1;
+    return querySum(node*2, l, r, tl, mid) + querySum(ode*2+1, l, r, mid+1, tr);
+}
+
+void update(int node, int tl, int tr, int k, int vl)
+{
+    if (tl == tr) st[node] = vl;
+    else {
+        int mid = (tl+tr)>>1;
+        if (k <= mid) update(node*2, tl, mid, k, vl);
+        else update(node*2+1, mid+1, tr, k, vl);
+        
+        st[node] = st[node*2] + st[node*2+1];
     }
 }
     
