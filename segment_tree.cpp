f820b6e1677f611ab7e94906ae2b509c4d94c145
Quan Duong <quanduong1396@gmail.com>
2023-04-07 10:03:01 +0700

    Create segment_tree.cpp

---
+++ segment_tree.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void build(int node, int l=1, int r=n)
+{
+    if (l == r) st[node] = arr[l];
+    else {
+        int mid = (l+r)>>1;
+        build(node*2, l, mid);
+        build(node*2+1, mod+1, r);
+        
+        st[node] = st[node*2] + st[node*2+1]; 
+    }
+}
+    
